1849fe799924312be369157a0ef38738ab585c33
softwareJengineer <52936239+softwareJengineer@users.noreply.github.com>
2025-08-05 21:26:27 -0500

    added new model with both animations, trying to control which gets played

--- frontend/src/pages/common/avatar/Avatar.tsx
+++ frontend/src/pages/common/avatar/Avatar.tsx
@@ -2,14 +2,20 @@ import { PerspectiveCamera  } from "@react-three/drei";
 import { Canvas             } from "@react-three/fiber";
 
 import Model2 from "./Model2";
+import Model3 from "./Model3";
+import { useState } from "react";
 
 // Avatar Model
 export default function Avatar() {
+    const [animation, setAnimation] = useState('SHAKE NO');
     return (
-        <Canvas>
-            <PerspectiveCamera makeDefault position={[0,  0, 10]} fov={50} />
-            <directionalLight              position={[0, 10, 10]} intensity={5} />
-            <Model2 />
-        </Canvas>
+        <div className="h-full w-full">
+            <Canvas>
+                <PerspectiveCamera makeDefault position={[0,  0, 10]} fov={50} />
+                <directionalLight              position={[0, 10, 10]} intensity={5} />
+                <Model3 animation={animation} setAnimation={setAnimation}/>
+            </Canvas>
+            <button className="p-4 border-1">Click me to cycle animation!</button>
+        </div>
     );
 }
