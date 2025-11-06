4187273870d3f8b78dca63243e0c754796e2dc3c
softwareJengineer <52936239+softwareJengineer@users.noreply.github.com>
2025-11-05 19:34:23 -0600

    update dependencies

--- frontend/src/pages/analysis/components/ChatExample.tsx
+++ frontend/src/pages/analysis/components/ChatExample.tsx
@@ -1,14 +1,14 @@
 import { Message } from "@/utils/misc/descriptions";
-import DOMPurify from 'dompurify';
+// import DOMPurify from 'dompurify';
 
 export default function ChatExample( {messages} : {messages: Message[]} ) {
     return (
         <div className="flex flex-col gap-2">
             {messages.map((message, idx) => {
-                const msg = DOMPurify.sanitize(message.text);
+                // const msg = DOMPurify.sanitize(message.text);
                 return (
                     <div key={idx} className="flex flex-row gap-2">
-                        <b>{message.sender}:</b> <div dangerouslySetInnerHTML={{ __html: msg}}></div>
+                        <b>{message.sender}:</b> <div dangerouslySetInnerHTML={{ __html: message.text}}></div>
                     </div>
                 )
             })}
